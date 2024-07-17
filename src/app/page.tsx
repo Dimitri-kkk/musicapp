8df98939eb0bb9a7b59ef67a123a6e67ff4b5ea8
lola212121 <nikaloladze06@gmail.com>
2024-07-17 21:23:58 +0400

    fix:heartButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+
+
 'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Heart } from "./Components/heart-button/heart-button";
+import HeartToggle from "./Components/heart-button/heartbutton/heart-button";
+// import { HeartButton } from "./Components/heart-button/heart-button";
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <Heart/>
+  
       </div>
-      
+
     </main>
   );
 }
+
+
+
+
+
+
+
+
