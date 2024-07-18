ccb6b236fa7375cd648f5a7a629ae992d401d165
Giorgi Tskhvaradze <giotskhvaradze3@gmail.com>
2024-07-18 15:20:54 +0400

    feat: Added Artist Hover

--- src/app/Components/ArtistCard/ArtistCard.tsx
+++ src/app/Components/ArtistCard/ArtistCard.tsx
@@ -3,17 +3,19 @@ import styles from './ArtistCard.module.scss';
 
 type Props = {
     title: string;
-    url: string
+    url: string 
     year: string
 }
 export const Artist = (props: Props) => {
     return(
+        <div className="parent-container">
         <div className={styles['Artist-Container']}>
-            <img src={props.url} alt={`${props.title} Picture`} className={styles['Artist-Picture']} />
-            <div className={styles['Artist-Info-Container']}>
-                <h1 className={styles['Artist-Name']}>{props.title}</h1>
-                <p className={styles['Artist-Year']}>{props.year}</p>
-            </div>
+          <img src={props.url} alt={`${props.title} Picture`} className={styles['Artist-Picture']} />
+          <div className={styles['Artist-Info-Container']}>
+            <h1 className={styles['Artist-Name']}>{props.title}</h1>
+            <p className={styles['Artist-Year']}>{props.year}</p>
+          </div>
         </div>
+      </div>
     )
-}
+}   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Artist } from "./Components/ArtistCard/ArtistCard";
+import MyImage from './Components/ArtistCard/Misha.png'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}> 
-        <Artist title="Coldplay" url="https://th.bing.com/th/id/OIP.avxhtQqr5FoubGjq4sIXugHaEo?rs=1&pid=ImgDetMain" year="2010"/>
+        <div className="styles.center">
+          <Artist title="Misha" url='https://i.ibb.co/4ppWzh1/image.png' year="2010"/>
+        </div>
       </div>
     </main>
   );
