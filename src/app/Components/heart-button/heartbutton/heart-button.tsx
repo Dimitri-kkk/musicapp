cbe3dd269c8290d72a58af6167da02330c960dc1
lola212121 <nikaloladze06@gmail.com>
2024-07-18 18:59:42 +0400

    fix: changed code heart-button

--- src/app/Components/heart-button/heartbutton/heart-button.tsx
+++ src/app/Components/heart-button/heartbutton/heart-button.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import Image from "next/image";
+import { useState } from "react";
 
 const HeartToggle = () => {
   const [liked, setLiked] = useState(false);
@@ -9,19 +10,12 @@ const HeartToggle = () => {
 
   return (
     <div onClick={toggleLike} style={{ cursor: 'pointer' }}>
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill={liked ? "white" : "none"}
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="feather feather-heart"
-      >
-        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 1 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-      </svg>
+      <Image
+        src={liked ? '/liked.svg' : '/not-liked.svg'}
+        alt="Heart Icon"
+        width={24}
+        height={24}
+      />
     </div>
   );
 };
