d1c852dbd1649c1be16d618693cd3e0eff2ff165
Giorgi Tskhvaradze <giotskhvaradze3@gmail.com>
2024-07-18 17:50:42 +0400

    Fix: Fixed Img alt

--- src/app/Components/ArtistCard/ArtistCard.tsx
+++ src/app/Components/ArtistCard/ArtistCard.tsx
@@ -11,7 +11,7 @@ export const Artist = (props: Props) => {
     return(
         <Link href='#' className={styles.ParentContainer}>
         <div className={styles.ArtistLink}>
-          <img src={props.url} alt={`${props.title} Picture`} className={styles.ArtistPicture} />
+          <img src={props.url} alt={Album} className={styles.ArtistPicture} />
           <div className={styles.ArtistInfoContainer}>
             <h1 className={styles.ArtistName}>{props.title}</h1>
             <p className={styles.ArtistYear}>{props.year}</p>
