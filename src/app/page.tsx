6923f9fbd64329e63ceb1e5b186484c1e50af737
George Tskhvaradze <152872563+GeorgeTskhvaradze15@users.noreply.github.com>
2024-07-19 18:12:17 +0400

    Revert "Feat: Added Reusable Button"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Button } from "./Components/Button/Button";
-import './Components/Button/Button.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <Button title="Secondary" mode="Big"/> 
-        <Button title="Primary" /> 
+      <div className={styles.description}> 
       </div>
     </main>
   );
