41c1718b80f6144497ef1a8e28aca358c7126bae
dimitri <dyaryarashvili@gmail.com>
2024-07-19 20:48:09 +0400

    refactor/team1-24/Fixed naming styles and usestate

--- src/app/Components/Playlist-button/PlaylistBtn.tsx
+++ src/app/Components/Playlist-button/PlaylistBtn.tsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
-import styles from './playlist-btn.module.css';
+import styles from './playlistBtn.module.css';
 
-interface PlaylistButtonProps {
+interface ButtonProps {
   initialText: string;
   mode: 'filled' | 'textOnly';
 }
 
-const PlaylistButton: React.FC<PlaylistButtonProps> = ({ initialText, mode }) => {
-  const [text, setText] = useState(initialText);
-
+const PlaylistButton: React.FC<ButtonProps> = ({ initialText, mode }) => {
   return (
     <button
       className={`${styles.btn} ${mode === 'filled' ? styles.filled : styles.textOnly}`}
     >
-      {text}
+      {initialText}
     </button>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import PlaylistButton from "./Components/Playlist-button/playlist-btn";
+import PlaylistButton from "./Components/Playlist-button/PlaylistBtn";
 
 export default function Home() {
   return (
