49b8347e439edd02974664ab8ce53db1ea085f14
Giorgi Tskhvaradze <giotskhvaradze3@gmail.com>
2024-07-21 15:47:53 +0400

    Fix: Fixed Errors With Merge Request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Artist } from "./Components/ArtistCard/ArtistCard";
-import MyImage from './Components/ArtistCard/Misha.png'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}> 
-        <div className="styles.center">
-          <Artist title="Misha" url='https://i.ibb.co/4ppWzh1/image.png' year="2010"/>
-        </div>
+      <div className={styles.description}>
+        <div className="styles.center"></div>
       </div>
     </main>
   );
