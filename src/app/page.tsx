c8a0ec2e5dff35671b59089fec61503ad9521efb
Giorgi Tskhvaradze <giotskhvaradze3@gmail.com>
2024-07-21 16:23:51 +0400

    Feat: Added Section Header Styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { SectionHeader } from "./Components/SectionHeader/SectionHeader";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}> 
+      <div className={styles.description}>
+        <SectionHeader title="Hello" />
       </div>
     </main>
   );
