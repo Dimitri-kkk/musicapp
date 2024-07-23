d17e88d55ec12079f280a696e3d4d39e75975ea5
dimitri <dyaryarashvili@gmail.com>
2024-07-23 13:31:49 +0400

    Feat: created chartCard component

--- src/app/Components/chartCard/chartCard.tsx
+++ src/app/Components/chartCard/chartCard.tsx
@@ -6,22 +6,24 @@ interface ChartProps {
     id?: string;
     image?: string;
     title?: string;
-    src: string
-  }
+    src: string;
+}
 
   // Gets fetched data from back-end server like image url, title and songs, but since we dont have back-server yet, I'm gonna use imaginary "/charts/"
-  const ChartCard: React.FC<ChartProps> = ({ id, image, title, src }) => {
+  const ChartCard: React.FC<ChartProps> = ({ id, title, src }) => {
     return (
       <Link className={styles.main} href={`/charts/${id}`}>
-        <div className={styles.chartImage}>
-          <Image
-            src={src} 
-            alt={title || 'Chart Image'} 
-            width={200} 
-            height={200} 
-          />
+        <div className={styles.chartContainer}>
+          <div className={styles.chartImage}>
+            <Image
+              src={src} 
+              alt={title || 'Chart Image'} 
+              width={200} 
+              height={200} 
+            />
           </div>
-          <h1>Chart title</h1>
+          <span>Chart title</span>
+        </div>
       </Link>
     );
   }
