39eca5455f2efb6962aca23586afd40548a02c6c
dimitri <dyaryarashvili@gmail.com>
2024-07-24 21:06:42 +0400

    Feat/Added MusicCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 'use client';
 import styles from "./page.module.css";
+import MusicCard from "./Components/MusicCard/MusicCard";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <MusicCard
+        src="./beatles.png"
+        title="Yellow"
+        artist="The beatles"
+      />
     </main>
   );
 }
